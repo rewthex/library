d712c42e2adcc40cb50ec0bfeb60e8d1849d31fe
Aaron <trapp.root@gmail.com>
2024-08-21 10:23:33 -0700

    Library functional minus ability to add book

--- script.js
+++ script.js
@@ -15,26 +15,35 @@ function Book(title, author, pages, read = false) {
 	(this.title = title),
 		(this.author = author),
 		(this.pages = pages),
-		(this.read = read ? "Has been read." : "Yet to be read");
+		(this.read = read);
 }
 
 function displayBooks() {
-    bookDisplay.innerHTML = "";
+	bookDisplay.innerHTML = "";
 	for (const [index, book] of myLibrary.entries()) {
 		const { title, author, pages, read } = book;
-		bookDisplay.innerHTML += `<div class="book" onclick="deleteBook(${index})">
-                                    ${title}
-                                    <button id="${index}">Delete</button>
+		bookDisplay.innerHTML += `<div class="book">
+                                    <div class="book-line"></div>
+                                    <h2 class="title">${title}</h2>
+                                    <h3 class="author">${author}</h3>
+                                    <div class="buttons">
+                                        <img src="/icons/delete.svg" height="30px" onclick="deleteBook(${index})"/>
+                                        <img src="/icons/${
+																					read ? "read.svg" : "notread.svg"
+																				}" height="30px" onclick="markRead(${index})"/>
+                                    </div>
                                   </div>`;
-	}
-	const deleteButtons = document.querySelectorAll("button");
-	for (let i = 0; i < deleteButtons.length; i++) {
-		deleteButtons[i].addEventListener("click", deleteBook);
 	}
 }
 
-function deleteBook(e) {
-    console.log(e)
+function deleteBook(index) {
+	myLibrary.splice(index, 1);
+	displayBooks();
+}
+
+function markRead(index) {
+	myLibrary[index].read = !myLibrary[index].read;
+    displayBooks();
 }
 
 function addBookToLibrary() {}
